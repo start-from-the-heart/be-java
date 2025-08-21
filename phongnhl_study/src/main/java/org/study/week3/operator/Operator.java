f50a58f155914c00a25ea97692828543623a5504
phongnhl <lamphong512999@gmail.com>
2025-08-22 00:13:15 +0700

    update study week 3 phongnhl

---
+++ phongnhl_study/src/main/java/org/study/week3/operator/Operator.java
@@ -0,0 +1,61 @@
+package org.study.week3.operator;
+
+public class Operator {
+	public static void main(String[] args) {
+		// gắn nhãn cho vòng for
+		outer: for(int i=0;i<3;i++) {
+			for(int j=0;j<3;j++) {
+				System.out.println("i:"+i+",j:"+j);
+				if(j==2) {
+					break outer;
+				}
+			}
+		}
+
+		// arithmetic & toán tử đơn giản
+		int a = 5, b = 2;
+		System.out.println(a + b); // 7
+		System.out.println(a - b); // 3
+		System.out.println(a * b); // 10
+		System.out.println(a / b); // 2 (integer division)
+		System.out.println(a % b); // 1
+
+		int x = 3;
+		System.out.println(4 + ++x); // 4 (prefix: tăng trước rồi in)
+		System.out.println(4 + x--); // 4 (postfix: in trước rồi giảm)
+		System.out.println(x);   // 3
+
+		// assigment operator
+		int z = 10;
+		z += 5;  // tương đương z = z + 5 → 15
+		z *= 2;  // 30
+//		z += 3.5;
+		System.out.println("z:"+z);
+
+		//Relational & Logical Operators
+		int c = 5, d = 10;
+
+		System.out.println(c < d);   // true
+		System.out.println(c == d);  // false
+		System.out.println(c != d);  // true
+
+		// với || và && thì nếu về trái đủ để quyết định còn & và | luôn thực hiện cả 2 vế
+		boolean g = true, h = false;
+		System.out.println("g && h:"+(g && h));  // false
+		System.out.println("g || h:"+(g || h));  // true
+		// cho dù về trái có đủ điều kiện thì | và & vẫn sẽ thực hiện vế phải
+		System.out.println("g & h:"+(g & h));  // false
+		System.out.println("g | h:"+(g | h));  // true
+
+		//Ternary Operator(toán tử 3 ngôi)
+		// 2 nhánh của ? và : phải có kiểu tương thích với kiểu trả về
+		int age = 20;
+		String result = (age >= 18) ? "Adult" : "Child";
+		System.out.println(result); // Adult
+
+		// instanceof
+		String s = "Hello";
+		System.out.println(s instanceof String); // true
+		System.out.println(s instanceof Object); // true
+	}
+}
---
+++ phongnhl_study/src/main/java/org/study/week3/operator/StudentManager.java
@@ -0,0 +1,144 @@
+package org.study.week3.operator;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Random;
+import java.util.Scanner;
+
+public class StudentManager {
+	static Scanner sc = new Scanner(System.in);
+	static Random	random = new Random();
+
+	public static void main(String[] args) {
+
+		System.out.println("Nhập số lượng sinh viên: ");
+		int n = sc.nextInt();
+
+		List<Student> students = new ArrayList<Student>();
+		// nhập danh sách sinh viên
+		for (int i = 1; i <= n; i++) {
+			Integer age = random.nextInt(16, 19);
+			Student student = new Student("Student"+i, age, random.nextFloat(4.0F,10.0F));
+			students.add(student);
+		}
+
+		int choice;
+		do {
+			System.out.println("\n--- MENU ---");
+			System.out.println("1. In danh sách");
+			System.out.println("2. Tìm sinh viên theo tên");
+			System.out.println("3. Cập nhật điểm");
+			System.out.println("4. Thoát");
+			System.out.print("Chọn: ");
+			choice = sc.nextInt();
+			sc.nextLine();
+			switch (choice) {
+			case 1:
+				printAll(students);
+				break;
+			case 2:
+				System.out.println("Nhập tên học sinh: ");
+				String name = sc.nextLine();
+				findStudent(name, students);
+				break;
+			case 3:
+				System.out.println("Nhập tên học sinh cần sửa: ");
+				String nameSeach = sc.nextLine();
+				System.out.println("Điểm mới: ");
+				int newGrade = sc.nextInt();
+				sc.nextLine();
+				updateScore(nameSeach, newGrade, students);
+				break;
+
+			}
+		}while(choice != 4 && choice > 0 && choice < 4 );
+	}
+
+	static void printAll(List<Student> students) {
+		StringBuilder sb = new StringBuilder();
+		float maxGrade = Float.MIN_VALUE;
+		float minGrade = Float.MAX_VALUE;
+		float sumGrade = Float.MIN_VALUE;
+		String maxName = "";
+		String minName = "";
+
+		for (Student student : students) {
+			if (maxGrade < student.getGrade()) {
+				maxGrade = student.getGrade();
+				maxName = student.getName();
+			}
+			if (minGrade > student.getGrade()) {
+				minGrade = student.getGrade();
+				minName = student.getName();
+			}
+			sumGrade +=  student.getGrade();
+		}
+
+		for (Student student : students) {
+			sb.append("Name: "+student.getName() + " ");
+			sb.append("Age: "+student.getAge() + " ");
+			sb.append("Grade: "+student.getGrade() + "\n");
+		}
+
+		float avgGrade = sumGrade /students.size();
+		System.out.println("\n--- Danh sách ---\n" + sb);
+		System.out.println("Điểm TB: " + avgGrade);
+		System.out.println("Cao nhất: " + maxName + " (" + maxGrade + ")");
+		System.out.println("Thấp nhất: " + minName + " (" + minGrade + ")");
+	}
+
+	static void findStudent(String name, List<Student> students) {
+		for (Student student : students) {
+			if (student.getName().equalsIgnoreCase(name)) {
+				System.out.println(student);
+				return;
+			}
+		}
+		System.out.println("Không tìm thấy!");
+	}
+
+	static void updateScore(String name, int newGrade, List<Student> students) {
+		for (Student student : students) {
+			if(student.getName().equalsIgnoreCase(name)) {
+				student.setGrade(newGrade);
+				System.out.println("Sửa thành công!");
+				return;
+			}
+		}
+		System.out.println("Không tìm thấy học sinh!!!");
+	}
+
+	static void deleteStudent(String name, List<Student> students) {
+// Không nên đùng enhanced for-each loop
+// Vòng for dùng iterator ngầm bên trong để duyệt
+// Khi dùng remove, list thay đổi trong khi iterator chưa kịp cập nhật => sung đột
+// Throw ConcurrentModificationException
+//		for (Student student : students) {
+//			if(student.getName().equalsIgnoreCase(name)) {
+//				students.remove(student);
+//			}
+//		}
+		// có 3 kiểu xóa an toàn
+		// dùng iterator trực tiếp
+		Iterator<Student> iterator = students.iterator();
+		while (iterator.hasNext()) {
+			Student student = iterator.next();
+			if (student.getName().equalsIgnoreCase(name)) {
+				iterator.remove();
+				return;
+			}
+		}
+		// dùng vòng for với chỉ số index
+		for(int i = 0; i < students.size(); i++) {
+			if(students.get(i).getName().equalsIgnoreCase(name)) {
+				students.remove(i);
+				i--;
+				return;
+			}
+		}
+
+		//dùng removeIf
+		students.removeIf(student -> student.getName().equalsIgnoreCase(name));
+	}
+}
